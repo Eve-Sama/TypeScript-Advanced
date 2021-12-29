c1fcdcb8dc18d575b72556d1fdca24963f4e6253
Eve-Sama <948832626@qq.com>
2021-12-29 22:13:42 +0800

    feat: 完成路由的初始化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'demo1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'demo1',
+    component: Demo1Component,
+  },
+  {
+    path: 'demo2',
+    component: Demo2Component,
+  },
+  {
+    path: '**',
+    redirectTo: 'demo1',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'TypeScript-Advanced';
+  cards = [
+    {
+      title: '复用代码',
+      url: '/demo1',
+    },
+    {
+      title: '复用代码2',
+      url: '/demo2',
+    },
+  ];
 }
