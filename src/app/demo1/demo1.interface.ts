eb405f14dbeb7bfefc333102e895bc63463fa582
Eve-Sama <948832626@qq.com>
2021-12-29 23:58:56 +0800

    feat: 添加demo1的优化方案

--- src/app/demo1/demo1.interface.ts
+++ src/app/demo1/demo1.interface.ts
@@ -8,3 +8,24 @@ export interface Field {
   src?: string;
   type: string;
 }
+
+// export interface BasicField {
+//   id: string;
+//   /** 字段的值 */
+//   value: string;
+//   type: FieldType;
+// }
+
+// export interface TextField extends BasicField {}
+// export interface DateField extends BasicField {
+//   /** 日期格式 */
+//   format: FieldFormat;
+// }
+// export interface ImageField extends BasicField {
+//   src: string;
+// }
+
+// export type FieldType = 'text' | 'date' | 'image';
+// export type FieldFormat = 'yyyy' | 'yyyy-mm' | 'yyyy-mm-dd';
+
+// export interface Field extends TextField, DateField, ImageField {}
